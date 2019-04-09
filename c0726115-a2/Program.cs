d208386c466ba62573dd42eba1a0a1bf2b45b6b8
visualstudio <visualstudio@125-PC36>
2019-04-09 17:37:18 -0400

    Lab3_final_assignment2

--- c0726115-a2/Program.cs
+++ c0726115-a2/Program.cs
@@ -5,17 +5,6 @@ using System.Text;
 
 
 using System.Threading.Tasks;
-
-
-
-
-
-
-
-
-
-
-
 namespace delegateAndEvents
 
 
@@ -32,23 +21,6 @@ namespace delegateAndEvents
 
 
 
-        static void Main(string[] args)
-
-
-
-        {
-
-
-
-
-
-
-
-            //Name 1 - Robin Bhatia
-
-
-
-            //ID - c0726115
 
 
 
@@ -57,77 +29,44 @@ namespace delegateAndEvents
 
 
 
+            //Name 1 - Inderjeet Singh
+
+
+
+            //ID - c0731909
 
 
 
 
 
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-        public class DelegateExcercises
-
-
-
-        {
-
-
-
-            public delegate void MyDelegate();
-
-
-
-            void Method1()
-
-
-
-            {
-
-
-
-
-
-
-
-                Console.WriteLine("Method 1");
-
-
-
-                Console.ReadLine();
+@@ -33,4 +31,14 @@ public void Method2();
 
 
 
             }
-
-
-
-            public void Method2()
-
-
-
-            {
-
-
-
-                MyDelegate myDelegate = new MyDelegate(Method1);
-
-
-
-                myDelegate();
-
-
-
-            }
-        }
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
