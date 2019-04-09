7a0716bdb1f3a53fb6efb2afabc9510aa816fbd5
visualstudio <visualstudio@125-PC36>
2019-04-09 17:09:21 -0400

    Lab1_Final_Assignment2

--- c0726115-a2/Program.cs
+++ c0726115-a2/Program.cs
@@ -1,72 +1,47 @@
-﻿using System.Reflection;
+﻿using System;
 
-using System.Runtime.CompilerServices;
+namespace Assignment02
+{
+    public class C0726115
+    {
+          static void Main(string[] args)
+          {
+            //Name 1 - Robin Bhatia
+            //ID - c0726115
+          }
+    public class DelegateExcercises
 
-using System.Runtime.InteropServices;
 
-
-
-// General Information about an assembly is controlled through the following
-
-// set of attributes. Change these attribute values to modify the information
-
-// associated with an assembly.
-
-[assembly: AssemblyTitle("Assignment -726115")]
-
-[assembly: AssemblyDescription("")]
-
-[assembly: AssemblyConfiguration("")]
-
-[assembly: AssemblyCompany("")]
-
-[assembly: AssemblyProduct("Assignment -726115")]
-
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-
-[assembly: AssemblyTrademark("")]
-
-[assembly: AssemblyCulture("")]
+        {
 
 
 
-// Setting ComVisible to false makes the types in this assembly not visible
-
-// to COM components.  If you need to access a type in this assembly from
-
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
+public delegate void MyDelegate();
 
 
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("a501a329-8c3f-4e5d-af9d-6c5535773c3c")]
+            void Method1()
 
 
 
-// Version information for an assembly consists of the following four values:
+            {
 
-//
 
-//      Major Version
 
-//      Minor Version
 
-//      Build Number
 
-//      Revision
 
-//
 
-// You can specify all the values or you can default the Build and Revision Numbers
+            }
 
-// by using the '*' as shown below:
 
-// [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
+        }
 
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
+
+    }
+
+
+
+}
