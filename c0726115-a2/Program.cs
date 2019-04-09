6224c6a8bb9f2e5992d42c00affc2c370a7b1c91
visualstudio <visualstudio@125-PC36>
2019-04-09 16:15:01 -0400

    first  commit

---
+++ c0726115-a2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c0726115_a2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
