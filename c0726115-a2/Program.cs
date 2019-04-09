4555bb2c132838efc152fab09d6b854de77f43da
visualstudio <visualstudio@125-PC36>
2019-04-09 16:15:54 -0400

    commit

--- c0726115-a2/Program.cs
+++ c0726115-a2/Program.cs
@@ -1,12 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Student name - Robin Bhatia   Student id - C0726115
+namespace Assignment2
 
-namespace c0726115_a2
 {
-    class Program
+
+    public class Program
+
     {
-        static void Main(string[] args)
+
+        public static void Main()
+
         {
-            Console.WriteLine("Hello World!");
+
+            DelegateExercises delegateExercises = new DelegateExercises();
+
+            delegateExercises.Method3();
+
+            Console.ReadLine();
+
+
+
         }
+
     }
+
+
+
+    public delegate void MyDelegate(ref int intValue);
+
+
+
+    public class DelegateExercises
+
+    {
+
+        void Method1(ref int intValue)
+
+        {
+
+            intValue = intValue + 5;
+
+            System.Console.WriteLine("Method1 " + intValue);
+
+        }
+
+
+
+        public void Method3()
+
+        {
+
+            MyDelegate myDelegate = new MyDelegate(Method1);
+
+            MyDelegate myDelegate1 = new MyDelegate(Method1);
+
+            MyDelegate myDelegate2 = myDelegate + myDelegate1;
+
+            int intParameter = 5;
+
+            myDelegate2(ref intParameter);
+
+        }
+
+    }
+
+
+
 }
+
