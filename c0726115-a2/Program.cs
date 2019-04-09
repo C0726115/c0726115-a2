733db20d84a0db103d3c3ac79a8473004c2f3ab0
visualstudio <visualstudio@125-PC36>
2019-04-09 17:34:50 -0400

    Lab2_Final_Asssignment2

--- c0726115-a2/Program.cs
+++ c0726115-a2/Program.cs
@@ -1,22 +1,89 @@
-﻿using System;
+﻿
+using System;
+using System.Text;
 
-namespace Assignment02
+
+
+using System.Threading.Tasks;
+
+
+
+
+
+
+
+
+
+
+
+namespace delegateAndEvents
+
+
+
 {
-    public class C0726115
+
+
+
+    class Program
+
+
+
     {
-          static void Main(string[] args)
-          {
-            //Name 1 - Robin Bhatia
-            //ID - c0726115
-          }
-    public class DelegateExcercises
+
+
+
+        static void Main(string[] args)
+
 
 
         {
 
 
 
-public delegate void MyDelegate();
+
+
+
+
+            //Name 1 - Robin Bhatia
+
+
+
+            //ID - c0726115
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+        public class DelegateExcercises
+
+
+
+        {
+
+
+
+            public delegate void MyDelegate();
 
 
 
@@ -32,16 +99,35 @@ public delegate void MyDelegate();
 
 
 
+                Console.WriteLine("Method 1");
+
+
+
+                Console.ReadLine();
+
+
+
             }
 
 
 
+            public void Method2()
+
+
+
+            {
+
+
+
+                MyDelegate myDelegate = new MyDelegate(Method1);
+
+
+
+                myDelegate();
+
+
+
+            }
         }
-
-
-
     }
-
-
-
 }
